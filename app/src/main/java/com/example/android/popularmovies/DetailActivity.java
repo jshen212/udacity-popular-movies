045fc118d59ebc3328fdd8762f86c99c8b67a33f
Jeffshen <jeffs1@Jeffs-MacBook-Pro.local>
2017-01-30 21:43:44 -0800

    [feature] added layout specs for movie detail activity

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -3,13 +3,18 @@ package com.example.android.popularmovies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.popularmovies.models.Movie;
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
-    private String mMovieDetails;
-    private TextView mMovieDisplayTitle;
+    private ImageView mMovieDetailPoster;
+    private TextView mMovieDetailTitle;
+    private TextView mMovieDetailRelDate;
+    private TextView mMovieDetailVoteAvg;
+    private TextView mMovieDetailSynopsis;
     private Movie parcedMovieObj;
 
     @Override
@@ -17,14 +22,22 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_detail);
 
-        mMovieDisplayTitle = (TextView) findViewById(R.id.movie_detail_title);
+        mMovieDetailPoster = (ImageView) findViewById(R.id.movie_detail_poster);
+        mMovieDetailTitle = (TextView) findViewById(R.id.movie_detail_title);
+        mMovieDetailRelDate = (TextView) findViewById(R.id.movie_detail_release_date);
+        mMovieDetailVoteAvg = (TextView) findViewById(R.id.movie_detail_vote_avg);
+        mMovieDetailSynopsis = (TextView) findViewById(R.id.movie_detail_synopsis);
+
         Intent intentThatStartedThisActivity = getIntent();
 
         if(intentThatStartedThisActivity != null) {
             if(intentThatStartedThisActivity.hasExtra("movieObject")) {
                 parcedMovieObj = (Movie) intentThatStartedThisActivity.getSerializableExtra("movieObject");
-//                mMovieDisplayTitle.setText(parcedMovieObj.toString());
-                mMovieDisplayTitle.setText(parcedMovieObj.getMovie_title());
+                Picasso.with(this).load(parcedMovieObj.getPoster_path()).into(mMovieDetailPoster);
+                mMovieDetailTitle.setText(parcedMovieObj.getMovie_title());
+                mMovieDetailRelDate.setText(parcedMovieObj.getRelease_date());
+                mMovieDetailVoteAvg.setText(parcedMovieObj.getVote_avg());
+                mMovieDetailSynopsis.setText(parcedMovieObj.getOverview());
             }
         }
 
